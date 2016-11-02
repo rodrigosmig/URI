7c36cea1ec9f15184f9f99a9864bb14ae2608903
rodrigo <rodrigo@rodrigo-mint>
2016-11-02 08:30:51 -0300

    Versão sem classe aninhada

--- src/EstradasEscuras.java
+++ src/EstradasEscuras.java
@@ -4,24 +4,14 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
-import java.util.LinkedList;
 
 public class EstradasEscuras {
 	
-	protected static class Node {
-		protected int valor;
-		protected boolean visita;
-		
-		public Node(int v, boolean f) {
-			valor = v;
-			visita = f;
-		}
-	}
-	
 	public static void main(String[] args) throws IOException {
+		
 		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
-
+		
 		String entrada = input.readLine();
 		
 		while(!entrada.equals("0 0")) {
@@ -31,12 +21,11 @@ public class EstradasEscuras {
 			int n = Integer.parseInt(entrada2[0]);
 			int m = Integer.parseInt(entrada2[1]);
 			
-			Node[][] matrizAdjacencia = new Node[n][n];
+			int[][] matrizAdjacencia = new int[n][n];
 			
 			for(int x = 0; x < matrizAdjacencia.length; x++) {
 				for(int y = 0; y < matrizAdjacencia.length; y++){
-					Node novoNode = new Node(Integer.MAX_VALUE, false);
-					matrizAdjacencia[x][y] = novoNode;
+					matrizAdjacencia[x][y] = Integer.MAX_VALUE;
 				}				
 			}
 			
@@ -46,8 +35,8 @@ public class EstradasEscuras {
 			for(int k = 0; k < m; k++) {
 				entrada = input.readLine();
 				String[] aresta = entrada.split(" ");
-				matrizAdjacencia[Integer.parseInt(aresta[0])][Integer.parseInt(aresta[1])].valor = Integer.parseInt(aresta[2]);
-				matrizAdjacencia[Integer.parseInt(aresta[1])][Integer.parseInt(aresta[0])].valor = Integer.parseInt(aresta[2]);
+				matrizAdjacencia[Integer.parseInt(aresta[0])][Integer.parseInt(aresta[1])] = Integer.parseInt(aresta[2]);
+				matrizAdjacencia[Integer.parseInt(aresta[1])][Integer.parseInt(aresta[0])] = Integer.parseInt(aresta[2]);
 				custoTotal += Integer.parseInt(aresta[2]);
 			}
 			
@@ -63,9 +52,9 @@ public class EstradasEscuras {
 
 	}
 	
-	public static int prim(Node[][] matriz) {
-		ArrayList<Integer> vertices = new ArrayList<Integer>(matriz.length);
-		LinkedList<Integer> resultado = new LinkedList<Integer>();
+	public static int prim(int[][] matriz) {
+		boolean[] visitados = new boolean[matriz.length];
+		ArrayList<Integer> vertices = new ArrayList<Integer>();
 		
 		int tempY;
 		int tempX;
@@ -73,6 +62,7 @@ public class EstradasEscuras {
 		int cont = 0;
 		int soma = 0;
 		
+		visitados[0] = true;
 		vertices.add(0);
 		
 		while(cont < matriz.length - 1) {
@@ -80,26 +70,24 @@ public class EstradasEscuras {
 			tempY = 0;
 			tempX = 0;
 			menorCaminho = Integer.MAX_VALUE;
-			
+
 			for(int x = 0; x < vertices.size(); x++) {
 				for(int y = 0; y < matriz.length; y++) {
-					if(matriz[vertices.get(x)][y].valor < menorCaminho && matriz[vertices.get(x)][y].visita == false) {
-						if(!vertices.contains(y)) {
-							menorCaminho = matriz[vertices.get(x)][y].valor;
+					if(visitados[y] == false) {
+						if(matriz[vertices.get(x)][y] < menorCaminho) {
+							menorCaminho = matriz[vertices.get(x)][y];
 							tempY = y;
 							tempX = vertices.get(x);
-						}					
+						}
 					}
-				}			
+				}
 			}
-			matriz[tempX][tempY].visita = true;
-			matriz[tempY][tempX].visita = true;
-			resultado.add(matriz[tempX][tempY].valor);
+			
 			vertices.add(tempY);
+			visitados[tempY] = true;
+			soma += menorCaminho;
+			matriz[tempX][tempY] = Integer.MAX_VALUE;
 			cont++;
-		}
-		for(int valor : resultado) {
-			soma += valor;
 		}
 		return soma;
 	}	
